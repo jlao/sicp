b391613eded316038e20baf860f3eb90292d582f
James Lao <james@jameslao.com>
2010-12-13 01:09:30 -0800

    Starting chapter 2

---
+++ chapter2.rkt
@@ -0,0 +1,28 @@
+
+(define (make-rat n d)
+  (let ((divisor (gcd n d)))
+    (cons (/ n divisor) (/ d divisor))))
+
+(define (numer r)
+  (car r))
+
+(define (denom r)
+  (cdr r))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
